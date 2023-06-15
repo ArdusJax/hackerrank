16ddbcddb6677ed9bcafff2613167acd1293c0a2
Chris O'Brian <mobiletread@gmail.com>
2023-06-14 22:42:00 -0700

    adding a method without abs or build in sort

--- sorted-squared-array/src/main.rs
+++ sorted-squared-array/src/main.rs
@@ -7,8 +7,29 @@ fn sorted_squared_array(array: Vec<i32>) -> Vec<i32> {
     a
 }
 
+fn sorted_squared_array_raw(array: Vec<i32>) -> Vec<i32> {
+    let mut start = 0;
+    let mut end = array.len() - 1;
+    let mut squares = Vec::new();
+    squares.resize(array.len(), 0);
+
+    for i in (0..=end).rev() {
+        if array[start] * -1 > array[end] {
+            squares[i] = array[start] * array[start];
+            start += 1;
+        } else {
+            squares[i] = array[end] * array[end];
+            end -= 1;
+        }
+    }
+
+    squares
+}
+
 fn main() {
     let test = vec![-5, -3, 1, 2, 4];
-    let answer = sorted_squared_array(test);
-    println!("{:?}", answer);
+    let answer = sorted_squared_array(test.clone());
+    let raw_answer = sorted_squared_array_raw(test);
+    println!("Answer with build in sort: {:?}", answer);
+    println!("Answer without build in sort: {:?}", raw_answer);
 }
